7b8acb7e069baa8d126e0f35d0c9ad852af92792
Mani246-sido <maninder13905@gmail.com>
2026-06-15 17:54:27 +0530

    added asynchandler

---
+++ backend/utils/aysncHandler.js
@@ -0,0 +1,11 @@
+//created this to handler web request ..thats it 
+
+
+
+const asyncHandler =(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err));
+
+    }
+}
+export {asyncHandler}
