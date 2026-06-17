6588c4e00b96acb03380da84ddf2cc7adce03d5c
Mani246-sido <maninder13905@gmail.com>
2026-06-17 15:25:07 +0530

    updated user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,22 +5,28 @@ const userSchema = new mongoose.Schema(
     name:{
         type:String,
         required:true,
+        trim:true,
     },
     email:{
         type:String,
         required:true,
+        lowercase:true,
         unique:true,
+        trim:true,
     },
     phoneNumber:{
         type:Number,
-        required:true,
         unique:true,
+        sparse:true, 
     },
     password:{
         type:String,
-        required:true,
+        select:false,
+        required:function(){
+            return !this.googleId && !this.githubId;
+        },
         unique:true,
-        length:8,
+        minlength:8,
         trim:true,
 
     },
@@ -28,13 +34,62 @@ const userSchema = new mongoose.Schema(
         type:String,
         required:true,
         unique:true,
+        lowercase:true,
+        trim:true,
     },
     profilePicture:{
-        type:String,
+        type:String,//cloudinary
+        default:"",
     },
     coverPicture:{
-        type:String,
+        type:String,//cloudinary
+        default:"",
     },
+    //Oauths
+    googleId:{ 
+        type:String,
+        default:null,
+    },
+    githubId:{
+        type:String,
+        default:null,
+    },
+    authProvider:{
+        type:String,
+        enum:["local","google","github"],
+        default:"local",
+    },
+
+    bio:{
+        type:String,
+        default:"",
+        maxlength:200,
+    },
+    skills:[{
+        type:String,
+        trim:true,
+    }],
+    experience:{
+        type:String,
+        enum:["Fresher","1-2 years","2-5 years","5+ years"],
+        default:"Fresher",
+    },
+    isAvailable:{
+        type:Boolean,
+        default:true,   
+    },
+    AvailableFor:[{
+        type:String,
+        enum:[
+            "Hackarthon",
+            "open source contribution",
+            "college project",
+            "startup",
+            "freelance",
+        ],
+    },
+],
+
     
     
 
