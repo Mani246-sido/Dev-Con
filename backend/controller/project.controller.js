f5a4cc4bb84ee7322d95c4d25edbb81997293ae7
Mani246-sido <maninder13905@gmail.com>
2026-06-24 15:38:41 +0530

    controller added

--- backend/controller/project.controller.js
+++ backend/controller/project.controller.js
@@ -1,5 +1,7 @@
 import {Project } from "../models/project.model.js"
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createProject = asyncHandler(async(req,res)=>{
     const {title, description, type, techStack, rolesNeeded, maxTeamSize, deadline, github, tags} = req.body;
@@ -23,4 +25,21 @@ const createProject = asyncHandler(async(req,res)=>{
     });
     return res.status(200).json(new ApiResponse(201,"Project created succesfully !", project));
 
-})
+});
+
+const editProject = asyncHandler(async(req,res)=>{
+    const project = await Project.findById(req.params.id);
+    if(!project){
+        throw new ApiError(404,"Project Not Found ");
+
+    }
+    if(project.owner.toString() !==req.user._id.toString()){
+        throw new ApiError(403,"Sorry, Only the project owner can edit this project :)")
+    }
+    const allowedFields = ["title", "description","type","status","techStack","rolesNeeded","maxTeamSize","deadline","github","tags"];
+    allowedFields.forEach((field)=>{
+        if(req.body[field] !== undefined) project[field]=req.body[field];
+    });
+    await project.save();
+    return res.status(200).json(new ApiResponse(200,"Project updated successfully", project));
+});
