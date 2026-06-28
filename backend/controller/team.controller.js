bd57ecd2a9f8a15a30bb9143be8a3634c3c835c0
Mani246-sido <maninder13905@gmail.com>
2026-06-28 13:53:38 +0530

    team controller started

--- backend/controller/team.controller.js
+++ backend/controller/team.controller.js
@@ -22,3 +22,32 @@ const createTeam = asyncHandler(async(req,res)=>{
     });
     return res.status(200).json(new ApiResponse(200,"Team created successfully",team));
 });
+
+const editTeams = asyncHandler(async(req,res)=>{
+    const team = Team.findById(req.params.id);
+    if(!team){
+        throw new ApiError(404,"Team Not found");
+    }
+    if(!team.creator.toString()!==req.user._id.toString()){
+        throw new ApiError(403,"Only the team creator can edit this team");
+    }
+    const allowedFields = ["name","description","skillsNeeded", "maxMembers", "visibility", "tags", "status"]
+    allowedFields.forEach((field)=>{
+        if(req.body[field]!==undefined) team[field]= req.body[field];
+    });
+    team.refershStatus();
+    await team.save();
+    return res.status(200).json(new ApiResponse(200,"Team updated successfully",team));
+});
+
+const getTeamDetails = asyncHandler(async(req,res)=>{
+    const team= await Team.findById(req.params.id)
+    .populate("creator", "name username profilePicture reputation")
+    .populate("members.user", "name username profilePicture skills reputation")
+    .populate("project", "title type status");
+
+    if(!team){
+        throw new ApiError(404,"Team Not found");
+    }
+    return res.status(200).json(new ApiResponse(200,"Team detaills fetched",team));
+});
